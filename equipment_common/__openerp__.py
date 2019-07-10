f1e7c84d55205ba718748409db8861b12a416c76
Andhitia Rama <andhitia.r@gmail.com>
2019-07-10 07:25:20 +0700

    [8.0.1.0.0] equipment_common

---
+++ equipment_common/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Common Equipment Feature",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/equipment_equipment_views.xml",
+    ],
+}
